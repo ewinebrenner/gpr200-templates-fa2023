859e39671adcb497794dde60fa02ae2627e7d6d3
Michael Lysik <michael.lysik@mymail.champlain.edu>
2023-10-30 15:31:59 -0400

    procGen.cpp and procGen.h initial commit

---
+++ core/cl/procGen.cpp
@@ -0,0 +1,25 @@
+#include "procGen.h"
+
+namespace celLib 
+{
+	ew::MeshData createSphere(float radius, int numSegments)
+	{
+		//TODO: fill out function
+		ew::MeshData returnValue;
+		return returnValue;
+	}
+
+	ew::MeshData createCylinder(float height, float radius, int numSegments)
+	{
+		//TODO: fill out function
+		ew::MeshData returnValue;
+		return returnValue;
+	}
+
+	ew::MeshData createPlane(float width, float height, int subdivisions)
+	{
+		//TODO: fill out function
+		ew::MeshData returnValue;
+		return returnValue;
+	}
+}
---
+++ core/cl/procGen.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../ew/mesh.h"
+
+namespace celLib 
+{
+	ew::MeshData createSphere(float radius, int numSegments);
+	ew::MeshData createCylinder(float height, float radius, int numSegments);
+	ew::MeshData createPlane(float width, float height, int subdivisions);
+}
